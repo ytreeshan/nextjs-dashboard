4bdff786a3df320a40d74dc57831e56b5b6920ce
ytreeshan <tjainarine@gmail.com>
2024-11-04 15:09:59 -0500

    making more edits and will

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,3 @@
-import { sql } from '@vercel/postgres';
 import {
   CustomerField,
   CustomersTableType,
@@ -8,18 +7,19 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { sql } from '@vercel/postgres';
 
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+     console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -30,13 +30,15 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
-
+   // Fetch the last 5 invoices, sorted by date
+const data = await sql<LatestInvoiceRaw>`
+SELECT invoices.amount, customers.name, customers.image_url, customers.email
+FROM invoices
+JOIN customers ON invoices.customer_id = customers.id
+ORDER BY invoices.date DESC
+LIMIT 5`;
+      
+      
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
