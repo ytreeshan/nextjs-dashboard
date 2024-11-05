92c6aeb876f004331dc4892bd9ecb21cae86e454
ytreeshan <44382924+ytreeshan@users.noreply.github.com>
2024-11-05 09:56:59 -0500

    Update page.tsx
    delelet in the import for sreach

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,4 @@
 import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
@@ -27,7 +26,6 @@ export default async function Page(props: {
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-     
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
