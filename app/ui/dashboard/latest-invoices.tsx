1a0563b5a09be1b46fb87c4ad701866e8469e785
ytreeshan <tjainarine@gmail.com>
2024-11-05 10:26:34 -0500

    mod some files hoping to fix errors

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -8,7 +8,9 @@ import { fetchLatestInvoices } from '@/app/lib/data';
 
 
 export default async function LatestInvoices() {
+
   const latestInvoices = await fetchLatestInvoices();
+  
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,30 +1,24 @@
 'use client';
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+//import { useSearchParams } from 'next/navigation';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
-
+ 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-
   function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
-    // console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
+   // console.log(term);
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-    console.log(term);
-  }, 300);
-
+  }
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -42,4 +36,4 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-}}
+}
