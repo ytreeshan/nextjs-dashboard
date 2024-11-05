18844cef47294d2956dd37528faea1e505290fd0
ytreeshan <tjainarine@gmail.com>
2024-11-05 09:44:50 -0500

    delete  <Search placeholder="Search invoices..."
    /> from invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -27,7 +27,7 @@ export default async function Page(props: {
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices..." />
+      
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
