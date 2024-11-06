c8ea5ec72454a3d9a22ec1be66b0d7d89a47cd11
ytreeshan <tjainarine@gmail.com>
2024-11-06 03:50:10 -0500

    fix lit errors and add new features

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,4 @@
-import Pagination from '@/app/ui/invoices/pagination';
+
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -25,14 +25,10 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
+      { <><Card title="Collected" value={totalPaidInvoices} type="collected" /><Card title="Pending" value={totalPendingInvoices} type="pending" /><Card title="Total Invoices" value={numberOfInvoices} type="invoices" /><Card
         title="Total Customers"
         value={numberOfCustomers}
-        type="customers"
-      /> */}
+        type="customers" /></> }
     </>
   );
 }
