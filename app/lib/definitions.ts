ea92004f9c6e34117ee26bbec6021305692383bd
ytreeshan <tjainarine@gmail.com>
2024-11-07 01:46:45 -0500

    added import for new component

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,6 +1,9 @@
 // This file contains type definitions for your data.
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
+
+import { ReactNode } from "react";
+
 // However, these types are generated automatically if you're using an ORM such as Prisma.
 export type User = {
   id: string;
